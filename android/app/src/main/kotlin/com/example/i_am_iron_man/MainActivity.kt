8d0cd59efb4d132d21655198f0fccbfe5a2980b6
Rajat Sharma <rajat.sharma4211@gmail.com>
2020-08-20 19:22:15 +0530

    initial commit

---
+++ android/app/src/main/kotlin/com/example/i_am_iron_man/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.i_am_iron_man
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.deepPurple[700],
+          title: Center(
+            child: Text(
+              'I am Iron Man',
+              style: TextStyle(
+                fontSize: 40,
+                foreground: Paint()
+                  ..style = PaintingStyle.stroke
+                  ..strokeWidth = 2
+                  ..color = Colors.deepOrangeAccent[200],
+              ),
+            ),
+          ),
+        ),
+        body: Container(
+          child: Center(
+            child: Image(
+              image: AssetImage('images/gifhy.gif'),
+              //height: 600,
+            ),
+          ),
+        ),
+        backgroundColor: Colors.lime[700],
+      ),
+    ),
+  );
+}
